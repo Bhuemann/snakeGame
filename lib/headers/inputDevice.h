dd3927dfdd9d098b44e8ed812fa17bce917e2303
bhuemann <benhuemann@gmail.com>
2016-02-14 17:41:29 +0000

    Added attributes and methods to inputDevice.h

---
+++ lib/headers/inputDevice.h
@@ -0,0 +1,17 @@
+
+class inputDevice {
+
+ private:
+	snake* snake;
+	int fileno;
+	string name;
+	int playerNum;
+
+ Public:
+	inputDevice(int fileno, int playerNum);
+	inputDevice(int playerNum);
+	
+
+
+
+}
